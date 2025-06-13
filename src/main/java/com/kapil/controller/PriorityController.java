c368dff6c6fb9e3ecd59c305a2b5af0abd5be6ba
kaplimonadi <kapilmonadi@gmail.com>
2025-06-13 13:52:03 +0530

    updated classes and pom.xml

---
+++ src/main/java/com/kapil/controller/PriorityController.java
@@ -0,0 +1,9 @@
+package com.kapil.controller;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/priority")
+@RequiredArgsConstructor
+public class PriorityController {
+}
